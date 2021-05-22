439d7b6f93c43724721c7165dba83a5e94941c83
Fax <dtauritis@gmail.com>
2021-05-23 00:54:38 +0200

    Added stylus to scene, gimped pen prefab

--- Assets/Scripts/PlayerManager.cs
+++ Assets/Scripts/PlayerManager.cs
@@ -6,10 +6,17 @@ public class PlayerManager : UdonSharpBehaviour
     [SerializeField] private PlayerUI playerUI = null;
     
     private int prompt = -1;
+    private int correctIndex = -1;
 
     public void SetPrompt(int index)
     {
         prompt = index;
         playerUI.SetPrompt(index);
     }
+
+    public void SetCorrectIndex(int index)
+    {
+        correctIndex = index;
+    }
+    
 }
