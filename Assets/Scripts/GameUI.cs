df875afdca5fa692bc0e31b274ecaf5ff8f78089
Fax <dtauritis@gmail.com>
2021-05-24 19:43:51 +0200

    Music!

--- Assets/Scripts/GameUI.cs
+++ Assets/Scripts/GameUI.cs
@@ -6,8 +6,15 @@ public class GameUI : UdonSharpBehaviour
 {
     [SerializeField] private TextMeshProUGUI textRound = null;
 
+    [SerializeField] private Animator animatorMusic = null;
+    private readonly int IsMusicRunning = Animator.StringToHash("IsMusicRunning");
+
     public void OnRoundChanged(int round)
     {
         textRound.text = "Round " + (round+1);
+        if (round >= 0 && round < 5)
+        {
+            animatorMusic.SetBool(IsMusicRunning, true);
+        }
     }
 }
