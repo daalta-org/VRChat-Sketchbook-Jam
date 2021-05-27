b4a248455b81efe9b740812ce96e069c20e82caa
Fax <dtauritis@gmail.com>
2021-05-27 18:27:37 +0200

    Added script for polling score numbers

---
+++ Assets/Scripts/ScoreScript.cs
@@ -0,0 +1,43 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+public class ScoreScript : UdonSharpBehaviour
+{
+    [SerializeField] private int[][] pointsGuess = null;
+    [SerializeField] private int[][] pointsBonus = null;
+
+    /// <summary>
+    /// Gets the points a player should receive
+    /// </summary>
+    /// <param name="playerCount">Absolute player count. Min 3, max 8</param>
+    /// <param name="placement">Placement. 0 is first, 1 is second, 2 is third...</param>
+    /// <returns>Points that player would receive, or -1 if the parameters were invalid.</returns>
+    public int GetGuessPoints(int playerCount, int placement)
+    {
+        if (playerCount < 3 || playerCount > 8)
+        {
+            return -1;
+        }
+
+        return pointsGuess[playerCount][placement];
+    }
+    
+    /// <summary>
+    /// Gets the points a player should receive
+    /// </summary>
+    /// <param name="playerCount">Absolute player count. Min 3, max 8</param>
+    /// <param name="placement">Placement. 0 is first, 1 is second, 2 is third...</param>
+    /// <returns>Points that player would receive, or -1 if the parameters were invalid.</returns>
+    public int GetBonusPoints(int playerCount, int placement)
+    {
+        if (playerCount < 3 || playerCount > 8)
+        {
+            return -1;
+        }
+
+        return pointsBonus[playerCount][placement];
+    }
+}
